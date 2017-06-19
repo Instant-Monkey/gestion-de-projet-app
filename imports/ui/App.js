67518b425670ea352afe8a711dcdef7a5eed38d9
sylvain <sylvain.laugier.net@gmail.com>
2017-06-19 22:50:50 +0200

    bettered grid layout

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -11,7 +11,10 @@ export default class App extends Component {
     return [
       {_id: 1, title: 'Postit number 1', text: 'text of postit 1'},
       {_id: 2, title: 'Postit number 2', text: 'text of postit 2'},
-      {_id: 3, title: 'Postit number 3', text: 'text of postit 3'}
+      {_id: 3, title: 'Postit number 3', text: 'text of postit 3'},
+      {_id: 4, title: 'Postit number 4', text: 'text of postit 4'},
+      {_id: 5, title: 'Postit number 5', text: 'text of postit 5'},
+      {_id: 6, title: 'Postit number 6', text: 'text of postit 6'}
     ]
   }
 
--- imports/ui/components/PostIt.js
+++ imports/ui/components/PostIt.js
@@ -2,28 +2,26 @@ import React, { Component} from "react";
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import {yellow100} from 'material-ui/styles/colors';
 
-const style = {
-  height: 100,
-  width: 100,
-  margin: 20,
-  textAlign: 'center',
-  display: 'inline-block',
+const postItStyle = {
   backgroundColor: yellow100
 };
 
 export default class PostIt extends Component {
   render() { 
     return(
-      <Card className="post-it-container" className="col s12 m7 l3">
-        <CardHeader
-          title={this.props.title}
-          actAsExpander={true}
-          showExpandableButton={true}
-        />
-        <cartText>
-          {this.props.text}
-        </cartText>
-      </Card>
+      <div className=" post-it-container col s12 m4 l3 xl2">
+        <Card style={postItStyle}>
+          <CardHeader
+            title={this.props.title}
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <cartText>
+            {this.props.text}
+          </cartText>
+        </Card>
+      </div>
+
     )
   }
 
